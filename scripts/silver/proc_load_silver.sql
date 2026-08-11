db50a1bb12efce485d0512c375f5f3fbbf452e7b
martynawieruszewska <martyna.wieruszewska@gmail.com>
2026-08-11 13:37:34 +0200

    loaded loc a101 into silver

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -125,6 +125,19 @@ case
 end as gen -- normalize gender
 from bronze.erp_cust_az12
 
+insert into silver.erp_loc_a101 (
+	cid,
+	cntry)
+select
+replace(cid, '-', '') as cid,
+case 
+	when trim(cntry) = 'DE' then 'Germany'
+	when trim(cntry) in ('US', 'USA') then 'United States'
+	when trim(cntry) = '' or cntry is null then 'n/a'
+	else trim(cntry)
+end as cntry -- normalize and handle missing or blank country codes
+from bronze.erp_loc_a101
+
 
 
 
