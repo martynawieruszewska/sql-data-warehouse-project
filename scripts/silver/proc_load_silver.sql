e544118d24ad3c577044c9386beac5db0cffd407
martynawieruszewska <martyna.wieruszewska@gmail.com>
2026-08-11 13:30:02 +0200

    loaded az12 into silver

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -63,3 +63,68 @@ prd_start_dt::date,
         ORDER BY prd_start_dt
     ) - INTERVAL '1 day')::date AS prd_end_dt -- correct date
 from bronze.crm_prd_info
+
+insert into silver.crm_sales_details (
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+)
+select 
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE
+    WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt::TEXT) != 8
+    THEN NULL
+    ELSE TO_DATE(sls_order_dt::TEXT, 'YYYYMMDD')
+END AS sls_order_dt,
+CASE
+    WHEN sls_ship_dt = 0 OR LENGTH(sls_ship_dt::TEXT) != 8
+    THEN NULL
+    ELSE TO_DATE(sls_ship_dt::TEXT, 'YYYYMMDD')
+END AS sls_ship_dt,
+CASE
+    WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt::TEXT) != 8
+    THEN NULL
+    ELSE TO_DATE(sls_due_dt::TEXT, 'YYYYMMDD')
+END AS sls_due_dt,
+case 
+	when sls_sales is null or sls_sales <= 0 or sls_sales != sls_quantity * abs(sls_price) then sls_quantity * abs(sls_price)
+	else sls_sales
+end as sls_sales,
+sls_quantity,
+case
+	when sls_price is null or sls_price <= 0 then sls_sales / coalesce(sls_quantity, 0)
+	else sls_price
+end as sls_price
+from bronze.crm_sales_details
+
+insert into silver.erp_cust_az12(
+	cid,
+	bdate,
+	gen)
+select
+case 
+	when cid like 'NAS%' then substring(cid from 4 for length(cid))
+	else cid
+end as cid, -- remove 'nas' pefix
+case 
+	when bdate > current_date then null 
+	else bdate
+end as bdate, -- set future bdates to null
+case 
+	when upper(trim(gen)) in ('F', 'FEMALE') then 'Female'
+	when upper(trim(gen)) in ('M', 'Male') then 'Male'
+	else 'n/a'
+end as gen -- normalize gender
+from bronze.erp_cust_az12
+
+
+
+
