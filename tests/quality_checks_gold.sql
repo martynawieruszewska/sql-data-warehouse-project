60a0e73244f10bb77eefcb30eb2060761baf1f79
martynawieruszewska <martyna.wieruszewska@gmail.com>
2026-08-14 13:40:28 +0200

    full gold layer connected

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -63,4 +63,13 @@ where prd_end_dt is null -- filtered out historical data
 )t group by prd_key
 having count(*) > 1
 
-select * from gold.dim_customers
+select * from gold.dim_products
+
+
+-- Sales
+select  * from gold.fact_sales f
+left join gold.dim_customers c
+on c.customer_key = f.customer_key
+left join gold.dim_products p
+on p.product_key = f.product_key
+where c.customer_key is null or p.product_key is null
